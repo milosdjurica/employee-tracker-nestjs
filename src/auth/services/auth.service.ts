9d82ae288fbf7edf6fa684958d76ae7837da7b23
milosdjurica <milosdjurica13@gmail.com>
2023-02-09 20:38:10 +0100

    register done

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -3,26 +3,24 @@ import { LoginDto, RegisterDto } from '../dto';
 import * as bcrypt from 'bcrypt';
 import { Tokens } from '../types';
 import { UserRepository } from '../auth.repository';
+import { ObjectId } from 'mongoose';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersRepository: UserRepository) {}
-
-  hashData(data: string) {
-    return bcrypt.hash(data, 10);
-  }
+  constructor(
+    private readonly usersRepository: UserRepository,
+    private jwtService: JwtService,
+  ) {}
 
   async register(dto: RegisterDto): Promise<Tokens> {
     const hash = await this.hashData(dto.password);
 
     const user = await this.usersRepository.create({ email: dto.email, hash });
+    const tokens = await this.getTokens(user['_id'], user.email);
+    await this.updateRtHash(user['_id'], tokens.refresh_token);
 
-
-
-
-
-    return { access_token: 'sad', refresh_token: 'sad' };
-    // return user.save();
+    return tokens;
   }
 
   login(dto: LoginDto) {}
@@ -30,4 +28,40 @@ export class AuthService {
   logOut() {}
 
   refreshTokens() {}
+
+  async updateRtHash(userId: ObjectId, rt: string) {
+    const hash = await this.hashData(rt);
+    await this.usersRepository.findOneAndUpdate(userId, { hashedRt: hash });
+  }
+
+  hashData(data: string) {
+    return bcrypt.hash(data, 10);
+  }
+
+  async getTokens(userId: ObjectId, email: string) {
+    const [at, rt] = await Promise.all([
+      this.jwtService.signAsync(
+        {
+          sub: userId,
+          email,
+        },
+        {
+          secret: process.env.JWT_AUTH_SECRET,
+          expiresIn: '50m',
+        },
+      ),
+      this.jwtService.signAsync(
+        {
+          sub: userId,
+          email,
+        },
+        {
+          secret: process.env.JWT_REFRESH_SECRET,
+          expiresIn: '7d',
+        },
+      ),
+    ]);
+
+    return { access_token: at, refresh_token: rt };
+  }
 }
